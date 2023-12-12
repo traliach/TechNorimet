94692442ba693e65dc176516846017663754c281
traliach <ali.achille.traore@gmail.com>
2023-12-12 16:48:45 -0500

    Iniitial website version

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for the button
+    document.getElementById('myButton').addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+
+    // Additional JavaScript can go here
+    // For example, handling navigation menu clicks, toggling sections, etc.
+});
